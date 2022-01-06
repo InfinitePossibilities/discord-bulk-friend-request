5f0cb76e9b946a6c937ffcec8fe0104778954034
InfiniteOne <smshelby01@gmail.com>
2022-01-05 18:11:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -23,91 +23,85 @@ function stats() {
 
 var interval = setInterval(() => {
 	current = iteration;
-	(() => {
-        var friendInput = document.getElementsByClassName("addFriendInputWrapper-3PjsbB")[0];
-        var friendInputHelper = document.getElementsByClassName("addFriendInputWrapper-3PjsbB")[0].getElementsByClassName("addFriendHint-3Y70FX")[0];
-        let focusLock = document.getElementsByClassName("focusLock-Ns3yie")[0];
+        (() => {
+            var friendInput = document.getElementsByClassName("addFriendInputWrapper-3PjsbB")[0];
+            var friendInputHelper = document.getElementsByClassName("addFriendInputWrapper-3PjsbB")[0].getElementsByClassName("addFriendHint-3Y70FX")[0];
+            let focusLock = document.getElementsByClassName("focusLock-Ns3yie")[0];
 
-        if (!friendInput) {
-            alert("Stopping! Please make sure you are in the Friends section!");
-            clearInterval(interval);
-            return;
-        }
-
-		// if (friendInput.children[1].disabled) {
-		// 	alert("Stopping! Please enter at least one character into the friend input!");
-		// 	clearInterval(interval);
-		// 	return;
-		// }
-		
-		if (focusLock) {
-			if (focusLock.ariaLabel == "Friend request failed") {
-				focusLock.getElementsByClassName("primaryButton-2BsGPp")[0].click();
-				failedRequests++
-			}
-            if (!focusLock.ariaLabel == "Friend request failed") {
-                console.log("An unhandled focusLock popup has occured! Stopping.");
+            if (!friendInput) {
+                alert("Stopping! Please make sure you are in the Friends section!");
                 clearInterval(interval);
                 return;
             }
-		}
-		
-		if (iteration < 10) {
-			current = "000" + iteration;
-            if (!friendInputHelper) {
-                friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+            
+            if (focusLock) {
+                if (focusLock.ariaLabel == "Friend request failed") {
+                    focusLock.getElementsByClassName("primaryButton-2BsGPp")[0].click();
+                    failedRequests++
+                }
+                if (!focusLock.ariaLabel == "Friend request failed") {
+                    console.log("An unhandled focusLock popup has occured! Stopping.");
+                    clearInterval(interval);
+                    return;
+                }
             }
+            
+            if (iteration < 10) {
+                current = "000" + iteration;
+                if (!friendInputHelper) {
+                    friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                }
 
-            friendInput.children[0].value = String(friend + "#" + current);
-            friendInput.children[1].value = String(friend + "#" + current);
-            friendInput.children[2].disabled = false;
-            friendInput.children[2].click();
+                friendInput.children[0].value = String(friend + "#" + current);
+                friendInput.children[1].value = String(friend + "#" + current);
+                friendInput.children[2].disabled = false;
+                friendInput.children[2].click();
 
-			console.log(current);
-			return;
-		}
-		if (iteration < 100) {
-			current = "00" + iteration;
-			if (!friendInputHelper) {
-                friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                console.log(current);
+                return;
             }
+            if (iteration < 100) {
+                current = "00" + iteration;
+                if (!friendInputHelper) {
+                    friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                }
 
-            friendInput.children[0].value = String(friend + "#" + current);
-            friendInput.children[1].value = String(friend + "#" + current);
-            friendInput.children[2].disabled = false;
-            friendInput.children[2].click();
+                friendInput.children[0].value = String(friend + "#" + current);
+                friendInput.children[1].value = String(friend + "#" + current);
+                friendInput.children[2].disabled = false;
+                friendInput.children[2].click();
 
-			console.log(current);
-			return;
-		}
-		if (iteration < 1000) {
-			current = "0" + iteration;
-			if (!friendInputHelper) {
-                friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                console.log(current);
+                return;
             }
+            if (iteration < 1000) {
+                current = "0" + iteration;
+                if (!friendInputHelper) {
+                    friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                }
 
-            friendInput.children[0].value = String(friend + "#" + current);
-            friendInput.children[1].value = String(friend + "#" + current);
-            friendInput.children[2].disabled = false;
-            friendInput.children[2].click();
+                friendInput.children[0].value = String(friend + "#" + current);
+                friendInput.children[1].value = String(friend + "#" + current);
+                friendInput.children[2].disabled = false;
+                friendInput.children[2].click();
 
-			console.log(current);
-			return;
-		}
-		if (iteration < 10000) {
-			if (!friendInputHelper) {
-                friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                console.log(current);
+                return;
             }
+            if (iteration < 10000) {
+                if (!friendInputHelper) {
+                    friendInput.insertBefore(friendInputHelperClone, friendInput.children[1]);
+                }
 
-            friendInput.children[0].value = String(friend + "#" + current);
-            friendInput.children[1].value = String(friend + "#" + current);
-            friendInput.children[2].disabled = false;
-            friendInput.children[2].click();
+                friendInput.children[0].value = String(friend + "#" + current);
+                friendInput.children[1].value = String(friend + "#" + current);
+                friendInput.children[2].disabled = false;
+                friendInput.children[2].click();
 
-			console.log(current);
-			return;
-		}
-	})()
+                console.log(current);
+                return;
+            }
+        })()
 	
 	if (iteration == maxIterations) {
 		clearInterval(interval);
